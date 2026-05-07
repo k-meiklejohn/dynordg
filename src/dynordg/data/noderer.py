dfb237b6d1b45590d127fb456708b38b7a09b0e9
Kyle Meiklejohn <kyle.meilklejohn314@gmail.com>
2026-05-07 14:03:39 +0100

    add citation in Noderer Data

--- src/dynordg/data/noderer.py
+++ src/dynordg/data/noderer.py
@@ -7,6 +7,12 @@ def load_score_dict(filename: str) -> dict:
             reader = csv.DictReader(f)
             return {row["sequence"]: float(row["efficiency"]) for row in reader}
         
+# The AUG and NON-AUG data are derived from:
+# Noderer, W. L. et al. Quantitative analysis of mammalian translation
+#  initiation sites by FACS‐seq. Mol. Syst. Biol. 10, 748 (2014) and
+# Diaz de Arce, A. J., Noderer, W. L. & Wang, C. L. Complete motif analysis of 
+# sequence requirements for translation initiation at non-AUG start codons. 
+# Nucleic Acids Res. 46, 985–994 (2018) respectively.
 
 SEQUENCE_TO_EFF_AUG = load_score_dict("aug.csv")
 SEQUENCE_TO_EFF_NON_AUG = load_score_dict("non_aug.csv")
