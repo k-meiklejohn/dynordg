50bdeb2cc0dbdbccba5bf94b3d1909cf62a1f836
Kyle Meiklejohn <kyle.meiklejohn314@gmail.com>
2026-08-27 11:00:09 +0100

    added Frameshift import

--- src/dynordg/__init__.py
+++ src/dynordg/__init__.py
@@ -1,4 +1,4 @@
 from .graph import FluxGraph, RiboSkeleton
-from .core import RiboNode, Transcript, State, Initiation, Retention, AllDrop, Termination, LoadScanning, IRES, ScanningDecay, Transition, TranslationDecay, TernaryComplexAssociation, ScanningFactorDissociation
+from .core import RiboNode, Transcript, State, Initiation, Retention, AllDrop, Termination, LoadScanning, IRES, ScanningDecay, Transition, TranslationDecay, TernaryComplexAssociation, ScanningFactorDissociation, Frameshift
 from .render import RiboGraphVis
 from .functions import quick_graph, quick_plot
